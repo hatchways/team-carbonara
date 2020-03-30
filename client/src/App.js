58dff4760992bb976da5d6ca0fc62e5f6397a6c7
Jenny Kam <jkam17@gmail.com>
2020-03-30 19:03:58 -0400

    clean up merges

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-<<<<<<< HEAD
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core";
+import { BrowserRouter, Route } from "react-router-dom";
 import { theme } from "./themes/theme";
 import ProfileSetup from "./pages/ProfileSetup";
-import LandingPage from "./pages/Landing";
-
-import "./App.css";
-=======
-import { theme } from './themes/theme';
->>>>>>> dev
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      <h1>Calendapp</h1>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Route path="/profile_settings" component={ProfileSetup} />
-        <Route path="/" component={LandingPage} />
-=======
-        <h1>Calendapp</h1>
->>>>>>> dev
-      </BrowserRouter>
+        <Route path="/profile_settings" component={ProfileSetup} />      </BrowserRouter>
     </MuiThemeProvider>
   );
 }
