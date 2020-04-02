b4c2f39d94e49bc9fbc81e8529d24ffbdd72a917
Raymond Lo <raymond.lo@yahoo.com>
2020-04-02 14:14:36 -0400

    implemented user routing

--- server/app.js
+++ server/app.js
@@ -1,24 +1,36 @@
 const createError = require('http-errors');
+const cors = require('cors');
 const express = require('express');
+const session = require('express-session');
 const { join } = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const pingRouter = require('./routes/ping');
+const userRouter = require('./routes/user');
 
 const { json, urlencoded } = express;
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
+//Since 1.5 cookie parser not necessary for express-session
+//module will directly read and write cookies
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/ping', pingRouter);
+//session middleware
+app.use(
+  session({
+    name: 'sid',
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+  }),
+);
+
+app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
