3e10659f2bbe1ff4abbbb206e5ad10a399344670
jncodes <midn.nelson@gmail.com>
2020-04-02 09:54:47 -0400

    Add required validator to user model

--- server/models/user.js
+++ server/models/user.js
@@ -5,15 +5,19 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     url: {
         type: String,
+        required: true,
     },
     timezone: {
         type: Map,
+        required: true,
     },
     availability: {
         type: Map,
+        required: true,
     },
     calendars: {
         type: Array,
+        required: true,
     },
 });
 
