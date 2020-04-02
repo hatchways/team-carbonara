5af2afeacefddf0f7ce426374b434b62cb499001
Jenny Kam <jkam17@gmail.com>
2020-04-01 23:39:58 -0400

    remove extra routes from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { theme } from './themes/theme';
 import OnBoarding from './pages/onBoarding/onBoarding';
 
-import ProfileSetup from './pages/onBoarding/ProfileSetup';
-import ConnectedPage from './pages/onBoarding/ConnectedPage';
-import AvailabilitySetup from './pages/onBoarding/AvailabilitySetup';
-
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
@@ -17,10 +13,6 @@ function App() {
         <Container>
           <h1>Calendapp</h1>
           <Route path="/onboarding" component={OnBoarding} />
-
-          <Route path="/profile_settings" component={ProfileSetup} />
-          <Route path="/connected" component={ConnectedPage} />
-          <Route path="/avail_settings" component={AvailabilitySetup} />
         </Container>
       </BrowserRouter>
     </MuiThemeProvider>
