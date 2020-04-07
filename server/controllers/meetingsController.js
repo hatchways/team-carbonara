1a3a3e97406a0af19c847b5ba657ed4aa63f9840
Jenny Kam <jkam17@gmail.com>
2020-04-06 22:13:20 -0400

    fix app.js require file, remove required family name from user model,  update sample.env, use prettier

--- server/controllers/meetingsController.js
+++ server/controllers/meetingsController.js
@@ -1,24 +1,21 @@
 const Meeting = require('../models/Meeting');
 
 const create = (req, res) => {
-    const newMeeting = new Meeting({
-        user_id: req.user.id,
-        duration: req.body.duration,
-    });
+  const newMeeting = new Meeting({
+    user_id: req.user.id,
+    duration: req.body.duration,
+  });
 
-    newMeeting
-        .save()
-        .then(() => res.status(200).json('Meeting saved'))
-        .catch(err => res.status(400).json(error));
-}
+  newMeeting
+    .save()
+    .then(() => res.status(200).json('Meeting saved'))
+    .catch((err) => res.status(400).json(error));
+};
 
 const index = (req, res) => {
-    
-    Meeting.find({ user: req.params.user_id })
-        .then(meetings => res.json(meetings))
-        .catch(err =>
-            res.status(404).json({ nomeetingsfound: 'No meetings found from that user' })
-        );
-}
+  Meeting.find({ user: req.params.user_id })
+    .then((meetings) => res.json(meetings))
+    .catch((err) => res.status(404).json({ nomeetingsfound: 'No meetings found from that user' }));
+};
 
 module.exports = { create, index };
--- server/models/Meeting.js
+++ server/models/Meeting.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const meetingSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-        required: true,
-    },
-    duration: {
-        type: Number,
-        required: true,
-    },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
 });
 
 const Meeting = mongoose.model('Meeting', meetingSchema);
