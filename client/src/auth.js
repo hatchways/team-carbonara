99f40a5f33d59900813e5175412ecfafa21f1607
Raymond Lo <raymond.lo@yahoo.com>
2020-04-07 21:05:06 -0400

    auth interface

---
+++ client/src/auth.js
@@ -0,0 +1,19 @@
+class Auth {
+  authenticated = false;
+
+  login(cb) {
+    this.authenticated = true;
+    cb();
+  }
+
+  logout(cb) {
+    this.authenticated = false;
+    cb();
+  }
+
+  isAuthenticated() {
+    return this.authenticated;
+  }
+}
+
+export default new Auth();
