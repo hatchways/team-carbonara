7eb495f463fe1f730c60650e597295bbaebcb4d7
Raymond Lo <raymond.lo@yahoo.com>
2020-04-07 21:04:53 -0400

    protected auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { theme } from './themes/theme';
 import Form from './components/Form/Form';
 import OnBoarding from './pages/OnBoarding';
-import handleFetchErrors from './utils/handleFetchErrors';
 import Dashboard from './pages/dashboard/dashboard';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 const stylesApp = (theme) => ({
   appName: {
@@ -20,49 +20,8 @@ const stylesApp = (theme) => ({
   },
 });
 
-//user arg returned from onSuccess
-function handleSuccessLogin(user) {
-  //send token to backend, verifiy and create session & or account
-  const idToken = user.getAuthResponse().id_token;
-
-  fetch('http://localhost:3001/api/user/login', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: idToken,
-  })
-    .then(handleFetchErrors)
-    .then((response) => console.log('token sent'))
-    .catch((error) => console.log(error));
-}
-
-function handleFailureLogin() {
-  console.log('Login failed');
-}
-
 function App(props) {
   const { classes } = props;
-
-  useEffect(() => {
-    window.gapi.load('auth2', () => {
-      //init GoogleAuth object
-      window.gapi.auth2
-        .init({
-          client_id: process.env.REACT_APP_CLIENT_ID,
-        })
-        .then((authObj) => {
-          //attach signin flow to button
-          authObj.attachClickHandler(
-            'googleButton',
-            { scope: 'https://www.googleapis.com/auth/calendar' },
-            handleSuccessLogin,
-            handleFailureLogin,
-          );
-        });
-    });
-  }, []);
 
   return (
     <React.Fragment>
@@ -70,15 +29,11 @@ function App(props) {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <main>
-            <h1 className={classes.appName}>
+            <h1 id="appName" className={classes.appName}>
               calend
               <span>app</span>
             </h1>
             <Switch>
-              {/* H1 rendering with dashboard, person integrating protected routes will need to fix */}
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
               <Redirect exact from="/" to="/login" />
               <Route path="/login">
                 <Form type="login" />
@@ -86,15 +41,10 @@ function App(props) {
               <Route path="/signup">
                 <Form type="signup" />
               </Route>
-              <Route path="/profile_settings">
-                <OnBoarding type="profile" activeStep={0} />
-              </Route>
-              <Route path="/confirm">
-                <OnBoarding type="confirm" activeStep={50} />
-              </Route>
-              <Route path="/availability">
-                <OnBoarding type="availability" activeStep={100} />
-              </Route>
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+              <PrivateRoute path="/profile_settings" type="profile" activeStep={0} component={OnBoarding} />
+              <PrivateRoute path="/confirm" type="confirm" activeStep={50} component={OnBoarding} />
+              <PrivateRoute path="/availability" type="availability" activeStep={100} component={OnBoarding} />
             </Switch>
           </main>
         </ThemeProvider>
