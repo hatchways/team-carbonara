52442a3988b9db1f70da096c3d9807c6cb040366
Raymond Lo <raymond.lo@yahoo.com>
2020-04-09 10:37:53 -0400

    embedded meeting document

--- server/models/User.js
+++ server/models/User.js
@@ -2,39 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-    },
-    given_name: {
-        type: String,
-        required: true,
-    },
-    family_name: {
-        type: String,
-        required: true,
-    },
-    sub: {
-        type: Number,
-        required: true,
-    },
-    picture: {
-        type: String,
-        required: true,
-    },
-    url: {
-        type: String,
-    },
-    timezone: {
-        type: Map,
-    },
-    availability: {
-        type: Map,
-        of: String,
-    },
-    calendars: {
-        type: Array,
-    },
+  email: {
+    type: String,
+    required: true,
+  },
+  given_name: {
+    type: String,
+    required: true,
+  },
+  family_name: {
+    type: String,
+    required: true,
+  },
+  sub: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+  },
+  timezone: {
+    type: Map,
+  },
+  availability: {
+    type: Map,
+    of: String,
+  },
+  calendars: {
+    type: Array,
+  },
+  meetings: [{ duration: Number, meetingName: String }],
 });
 
 const User = mongoose.model('User', userSchema);
