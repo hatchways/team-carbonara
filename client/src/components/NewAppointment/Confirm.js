e0874437d1effe7705eab8d54cb0665865335b76
Jenny Kam <jkam17@gmail.com>
2020-04-14 22:51:36 -0400

    add error display for required inputs, add materialui components, display info with test props

--- client/src/components/NewAppointment/Confirm.js
+++ client/src/components/NewAppointment/Confirm.js
@@ -1,49 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper, Divider } from '@material-ui/core';
-import { TextField, InputAdornment } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import Grid from '@material-ui/core/Grid';
+import { TextField, TextareaAutosize, Button, IconButton } from '@material-ui/core';
+import { FiClock, FiArrowLeftCircle } from 'react-icons/fi';
+import { FaRegCalendarCheck, FaGlobeAmericas } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import * as moment from 'moment-timezone';
 
 const styles = {
   paper: {
     width: '80%',
-    height: '500px',
+    height: '600px',
     display: 'flex',
     justifyContent: 'space-between',
     flexDirection: 'row',
     margin: '0 auto',
-    padding: '1.5rem',
+    marginBottom: '200px',
   },
   meetingInfo: {
     flex: '0 0 30%',
+    padding: '1.5rem',
+    borderRight: '1px solid #eee',
   },
   confirmForm: {
     flex: '0 0 70%',
+    padding: '1.5rem',
+    flexDirection: 'column',
   },
 };
 
 function Confirm(props) {
-  const { classes, setUrl, setTimeZone } = props;
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May'];
+  const clientTz = moment.tz.guess();
 
+  const [nameField, setName] = useState({ name: '', error: false, errorText: '' });
+  const [emailField, setEmail] = useState({ email: '', error: false, errorText: '' });
+
+  const handleName = async (e) => {
+    e.target.value === ''
+      ? setName({ name: '', error: true, errorText: 'Name is required.' })
+      : setName({ name: e.target.value, error: false, errorText: '' });
+  };
+
+  const handleEmail = async (e) => {
+    e.target.value === ''
+      ? setEmail({ email: '', error: true, errorText: 'Email is required.' })
+      : setEmail({ email: e.target.value, error: false, errorText: '' });
+  };
+  console.log(nameField, emailField);
+
+  const { classes, user, meetingName, meetTime, apptTime } = props;
+  const appt = moment(apptTime);
   return (
     <Paper elevation={6} className={classes.paper}>
       <div className={classes.meetingInfo}>
-        <div>Back button?</div>
-        <h4>Meeting with: name</h4>
-        <h1>Meeting name (30min meeting)</h1>
-        <div>Clock icon, 30min</div>
-        <div>Schedule icon, 9:00am - 9:45am, Wednesday, May 13, 2020</div>
-        <div>World icon Eastern Time - US & Canada</div>
+        <IconButton aria-label="back-to-scheduler">
+          <FiArrowLeftCircle />
+        </IconButton>
+        <h4>Meeting with {user}</h4>
+        <h1>{meetingName}</h1>
+        <div>
+          <FiClock /> {meetTime}min
+        </div>
+        <div>
+          <FaRegCalendarCheck /> {appt.format('hh:mma')} - {appt.add(meetTime, 'm').format('hh:mma')},{' '}
+          {weekdays[appt.day()]}, {months[appt.month()]} {appt.date()}, {appt.year()}
+        </div>
+        <div>
+          <FaGlobeAmericas /> {clientTz + ' (GMT' + moment.tz(clientTz).format('Z') + ')'}
+        </div>
       </div>
+
       <Divider orientation="vertical" />
+
       <div className={classes.confirmForm}>
-        <div>Name Field</div>
-        <div>Email Field</div>
-        <div>Comments Textarea</div>
-        <div>Schedule Event Button</div>
+        <h2>Enter Details</h2>
+        <div>
+          <div>Name:</div>
+          <TextField
+            required
+            error={nameField.error}
+            helperText={nameField.errorText}
+            id="name-field"
+            variant="outlined"
+            name="name"
+            onChange={handleName}
+          />
+        </div>
+        <div>
+          <div>Email: </div>
+          <TextField
+            required
+            error={emailField.error}
+            helperText={emailField.errorText}
+            id="email-field"
+            variant="outlined"
+            name="email"
+            onChange={handleEmail}
+          />
+        </div>
+
+        <div>Meeting Notes: </div>
+        <div>
+          <TextareaAutosize
+            className={classes.texarea}
+            aria-label="meeting-comments"
+            rowsMin={7}
+            placeholder="Minimum 3 rows"
+          />
+        </div>
+        <Button variant="contained" color="primary">
+          Schedule Event
+        </Button>
       </div>
     </Paper>
   );
@@ -51,8 +120,6 @@ function Confirm(props) {
 
 Confirm.propTypes = {
   classes: PropTypes.object.isRequired,
-  setUrl: PropTypes.func.isRequired,
-  setTimeZone: PropTypes.func.isRequired,
 };
 
 export default withStyles(styles)(Confirm);
