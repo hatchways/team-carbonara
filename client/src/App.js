ebd334d2ba43269e3e1936ef54e48de366ad3180
Raymond Lo <raymond.lo@yahoo.com>
2020-04-16 17:04:48 -0400

    added calendar route

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,14 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { theme } from './themes/theme';
 import Form from './components/Form/Form';
 import OnBoarding from './pages/OnBoarding';
-import Dashboard from './pages/dashboard/dashboard';
+import Dashboard from './pages/Dashboard/Dashboard';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Calendar from './pages/Calendar/Calendar';
 
 const stylesApp = (theme) => ({
   appName: {
     margin: '5rem 0 3rem 0',
     color: '#ff6d00',
-    // color: theme.palette.primary.main,
     textAlign: 'center',
     '& span': {
       color: 'rgba(0, 0, 0, 0.87)',
@@ -26,25 +26,28 @@ function App({ classes }) {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <main>
-            <h1 id="appName" className={classes.appName}>
-              calend
-              <span>app</span>
-            </h1>
-            <Switch>
-              <Redirect exact from="/" to="/login" />
-              <Route path="/login">
-                <Form type="login" />
-              </Route>
-              <Route path="/signup">
-                <Form type="signup" />
-              </Route>
-              <PrivateRoute path="/dashboard" component={Dashboard} />
-              <PrivateRoute path="/profile_settings" type="profile" activeStep={0} component={OnBoarding} />
-              <PrivateRoute path="/confirm" type="confirm" activeStep={50} component={OnBoarding} />
-              <PrivateRoute path="/availability" type="availability" activeStep={100} component={OnBoarding} />
-            </Switch>
-          </main>
+          <Switch>
+            <Route path="/:url/:eventDuration" component={Calendar} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <React.Fragment>
+              <h1 id="appName" className={classes.appName}>
+                calend
+                <span>app</span>
+              </h1>
+              <Switch>
+                <Redirect exact from="/" to="/login" />
+                <Route path="/login">
+                  <Form type="login" />
+                </Route>
+                <Route path="/signup">
+                  <Form type="signup" />
+                </Route>
+                <PrivateRoute path="/profile_settings" type="profile" activeStep={0} component={OnBoarding} />
+                <PrivateRoute path="/confirm" type="confirm" activeStep={50} component={OnBoarding} />
+                <PrivateRoute path="/availability" type="availability" activeStep={100} component={OnBoarding} />
+              </Switch>
+            </React.Fragment>
+          </Switch>
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
