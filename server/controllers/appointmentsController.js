a10f7f662035df1aad2e5465dc3d7c5754766e1b
Jenny Kam <jkam17@gmail.com>
2020-04-16 19:08:56 -0400

    relate appointments model to user model,  create appt w/user, separate email helper

--- server/controllers/appointmentsController.js
+++ server/controllers/appointmentsController.js
@@ -1,53 +1,36 @@
 const Appointment = require('../models/Appointment');
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const User = require('../models/User');
+const { emailUserNewAppt } = require('../utils/emailHelper.js');
 
-const create = (req, res) => {
+const create = async (req, res) => {
   console.log(req.body);
-  const { guestName, guestEmail, guestComment, guestTz, meetingName, meetTime, apptTime } = req.body;
-  const newAppointment = new Appointment({
-    guestName: guestName,
-    guestEmail: guestEmail,
-    guestComment: guestComment,
-    guestTz: guestTz,
-    meetingName: meetingName,
-    meetTime: meetTime,
-    apptTime: apptTime,
-  });
+  const { guestName, guestEmail, guestComment, guestTz, meetingName, meetTime, apptTime, url } = req.body;
 
   try {
-    const appt = newAppointment.save();
+    const user = await User.findOne({ url });
+
+    const newAppointment = new Appointment({
+      user: user._id,
+      guestName: guestName,
+      guestEmail: guestEmail,
+      guestComment: guestComment,
+      guestTz: guestTz,
+      meetingName: meetingName,
+      meetTime: meetTime,
+      apptTime: apptTime,
+    });
+
+    const appt = await newAppointment.save();
     res.status(201).send(appt);
-  } catch (error) {
-    res.status(400).json(error);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
   }
 
-  // const msg = {
-  //   to: userEmail,
-  //   from: process.env.SENDER_EMAIL_ADDRESS,
-  //   templateId: 'd-6125845abb24432b8af269d996fac682',
-  //   dynamic_template_data: {
-  //     name: userName,
-  //     guest: {
-  //       name: guestName,
-  //       email: guestEmail,
-  //       timezone: timezone,
-  //       comments: guestComments,
-  //     },
-  //     meetingName: meetingName,
-  //     eventTime: '09:00am - Wednesday, May 20, 2020 (America/New York GMT-4:00)',
-  //   },
-  // };
-  // try {
-  //   sgMail.send(msg);
-  // } catch (error) {
-  //   console.error(error);
-  //
-  //   if (error.response) {
-  //     console.error(error.response.body);
-  //   }
-  // }
-  res.send('Done');
+  //find user
+  //send appt to gcal
+
+  // emailUserNewAppt(user.email, user.given_name, guestName, guestEmail, guestTz, guestComment, meetingName);
 };
 
 const index = (req, res) => {
