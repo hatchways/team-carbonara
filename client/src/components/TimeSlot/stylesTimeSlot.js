fe7171af431f1fc6d1f1a04d20b8acd659673995
Raymond Lo <raymond.lo@yahoo.com>
2020-04-16 17:02:26 -0400

    time slot styles

---
+++ client/src/components/TimeSlot/stylesTimeSlot.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { nominalTypeHack } from 'prop-types';
+
+const useStylesTimeSlot = makeStyles({
+  container: {
+    display: 'flex',
+  },
+  button: {
+    flex: '1',
+    width: '100%',
+    border: '1px solid orange',
+    fontWeight: 'bold',
+    color: 'orange',
+    height: '52px',
+    backgroundColor: 'Transparent',
+    borderRadius: '3px',
+    fontSize: '1rem',
+    margin: '5px',
+    cursor: 'pointer',
+    '&:hover': {
+      border: '2px solid darkorange',
+    },
+  },
+  confirmBtn: {
+    flex: '1',
+    margin: '5px',
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    height: '52px',
+    color: 'orange',
+    backgroundColor: '#484848',
+    borderRadius: '3px',
+    border: 'none',
+  },
+});
+
+export default useStylesTimeSlot;
