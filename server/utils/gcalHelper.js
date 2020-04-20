d1223e75dfa4e2b15e6390681440ccc9527067ff
Jenny Kam <jkam17@gmail.com>
2020-04-20 02:29:57 -0400

    set in token for freebusy, checked timeslot avail fetch, algo bug fix, clean code

--- server/utils/gcalHelper.js
+++ server/utils/gcalHelper.js
@@ -4,32 +4,39 @@ const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.C
 oauth2Client.generateAuthUrl({ access_type: 'offline' });
 
 async function getFreebusy(token, startISO, endISO) {
-  // oauth2Client.setCredentials({
-  //   access_token: token,
-  //   refresh_token: process.env.CODE,
-  // });
-
-  const resp = await calendar.freebusy
-    .query({
+  oauth2Client.setCredentials({
+    access_token: token,
+    // refresh_token: user.refresh_token,
+  });
+  oauth2Client.on('tokens', (tokens) => {
+    console.log(tokens);
+    //   await oauth2Client.setCredentials(tokens);
+    if (tokens.refresh_token) {
+      // store the refresh_token in my database!
+      console.log(tokens.refresh_token);
+    }
+    console.log(tokens.access_token);
+  });
+  try {
+    const resp = await calendar.freebusy.query({
       auth: oauth2Client,
       resource: {
         items: [{ id: 'primary' }],
         timeMin: startISO,
         timeMax: endISO,
       },
-    })
-    .then((resp) => resp.data.calendars.primary.busy)
-    .catch((err) => console.log('ERROR HERE', err));
-
-  return resp;
+    });
+    return resp.data.calendars.primary.busy;
+  } catch (err) {
+    throw ('Error at freebusy', err);
+  }
 }
 
 async function insertEvent(token, startISO, endISO, timeZone, meetingName, guestEmail, guestName, guestComment) {
   oauth2Client.setCredentials({
     access_token: token,
-    // refresh_token: process.env.CODE,
+    // refresh_token: user.refresh_token,
   });
-  console.log('IM FIRST', oauth2Client);
 
   oauth2Client.on('tokens', (tokens) => {
     console.log(tokens);
@@ -64,7 +71,7 @@ async function insertEvent(token, startISO, endISO, timeZone, meetingName, guest
       resource: event,
     });
   } catch (err) {
-    throw Error(err);
+    throw ('Error at insert', err);
   }
 }
 
