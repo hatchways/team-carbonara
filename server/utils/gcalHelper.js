0c3090314089d67fc46028f9ac781f1024935640
Jenny Kam <jkam17@gmail.com>
2020-04-20 14:31:59 -0400

    add refresh tokens to apptmt/avail controllers, gcal helpers

--- server/utils/gcalHelper.js
+++ server/utils/gcalHelper.js
@@ -1,23 +1,29 @@
+const User = require('../models/User');
 const { google } = require('googleapis');
 const calendar = google.calendar('v3');
 const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, `postmessage`);
 oauth2Client.generateAuthUrl({ access_type: 'offline' });
 
-async function getFreebusy(token, startISO, endISO) {
+async function getFreebusy(access, refresh, startISO, endISO, url) {
   oauth2Client.setCredentials({
-    access_token: token,
-    // refresh_token: user.refresh_token,
+    access_token: access,
+    refresh_token: refresh,
   });
+
   oauth2Client.on('tokens', (tokens) => {
     console.log(tokens);
     //   await oauth2Client.setCredentials(tokens);
     if (tokens.refresh_token) {
       // store the refresh_token in my database!
-      console.log(tokens.refresh_token);
+      const user = User.findOne({ url });
+      user.refresh_token = tokens.refresh_token;
+      user.save();
+      console.log('oy refresh', tokens.refresh_token);
     }
-    console.log(tokens.access_token);
+    console.log('oy access', tokens.access_token);
   });
   try {
+    console.log('before', oauth2Client.credentials);
     const resp = await calendar.freebusy.query({
       auth: oauth2Client,
       resource: {
@@ -26,29 +32,44 @@ async function getFreebusy(token, startISO, endISO) {
         timeMax: endISO,
       },
     });
+    console.log('after', oauth2Client.credentials);
+
     return resp.data.calendars.primary.busy;
   } catch (err) {
     throw ('Error at freebusy', err);
   }
 }
 
-async function insertEvent(token, startISO, endISO, timeZone, meetingName, guestEmail, guestName, guestComment) {
+async function insertEvent(
+  access,
+  refresh,
+  startISO,
+  endISO,
+  timeZone,
+  meetingName,
+  guestEmail,
+  guestName,
+  guestComment,
+  url,
+) {
   oauth2Client.setCredentials({
-    access_token: token,
-    // refresh_token: user.refresh_token,
+    access_token: access,
+    refresh_token: refresh,
   });
 
   oauth2Client.on('tokens', (tokens) => {
     console.log(tokens);
-    //   await oauth2Client.setCredentials(tokens);
     if (tokens.refresh_token) {
       // store the refresh_token in my database!
-      console.log(tokens.refresh_token);
+      const user = User.findOne({ url });
+      user.refresh_token = tokens.refresh_token;
+      user.save();
+      // console.log(tokens.refresh_token);
     }
-    console.log(tokens.access_token);
+    // console.log(tokens.access_token);
   });
 
-  var event = {
+  const event = {
     summary: meetingName,
     start: {
       dateTime: startISO,
